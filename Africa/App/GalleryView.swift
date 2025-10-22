2d31b996734cc579cb92a40fe2af7028f1ddb51a
Ghenadie Isacenco <isacenco@gmail.com>
2025-10-22 13:24:04 +0200

    Json Decoder

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 struct GalleryView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
     var body: some View {
         Text("Gallery")
     }
 }
 
+// MARK: - PREVIEW
 #Preview {
     GalleryView()
 }
