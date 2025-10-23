d4752190b4b192aaa025a21d72cec1147cd43d6a
Ghenadie Isacenco <isacenco@gmail.com>
2025-10-23 09:25:08 +0200

    Create Complex Map Part 1

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,13 +6,42 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
     // MARK: - PROPERTIES
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
     
     // MARK: - BODY
     var body: some View {
-        Text("Map")
+        
+        // MARK: - No1 basic map
+        //Map(coordinateRegion: $region)
+        
+        // MARK: - No2 Map with anotations
+        Map(coordinateRegion: $region, annotationItems: locations) { item in
+            // (A) PIN: OLD STYLE (always static)
+            //MapPin(coordinate: item.location, tint: .accentColor)
+            
+            // (B) MARKER: NEW STILE (always static)
+            //MapMarker(coordinate: item.location, tint: .accentColor)
+            
+            // (C) CUSTOM BASIC ANNOTATION (could be interactive)
+            
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+            } //: ANNOTATION
+        }
     }
 }
 
